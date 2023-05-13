d1ac204f6e01ddc08afd928eeca4a0e6f60d9425
Piotr Brzozowski <piotr.brzozowski.dev@gmail.com>
2023-05-13 15:58:02 +0200

    test(paa) add Pokemon list controller integration test

---
+++ src/test/java/pl/sdaacademy/pokemonapi/pokemonlist/PokemonListControllerIntegrationTest.java
@@ -0,0 +1,42 @@
+package pl.sdaacademy.pokemonapi.pokemonlist;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import pl.sdaacademy.pokemonapi.PokemonApiApplication;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+classes = PokemonApiApplication.class)
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+public class PokemonListControllerIntegrationTest {
+
+    @Autowired
+    private PokemonListItemRepository pokemonListItemRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void given_application_started_when_send_GET_pokemon_list_request_then_pokemon_list_should_be_returned() throws Exception {
+        //given
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.get("/pokemon/list")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.not(Matchers.emptyArray())));
+
+        Assertions.assertTrue(pokemonListItemRepository.count() != 0);
+    }
+}
