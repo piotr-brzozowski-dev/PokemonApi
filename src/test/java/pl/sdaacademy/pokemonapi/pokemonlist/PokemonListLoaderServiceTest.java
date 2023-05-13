f58b542fcb4dc950b797b9a54e44cff5de4dd4a7
Piotr Brzozowski <piotr.brzozowski.dev@gmail.com>
2023-05-13 11:58:19 +0200

    test(paa): add Pokemon list loader service unit test

--- src/test/java/pl/sdaacademy/pokemonapi/pokemonlist/PokemonListLoaderServiceTest.java
+++ src/test/java/pl/sdaacademy/pokemonapi/pokemonlist/PokemonListLoaderServiceTest.java
@@ -25,4 +25,53 @@ class PokemonListLoaderServiceTest {
         //then
         Mockito.verify(fetchPokemonListUseCase, Mockito.times(1)).execute();
     }
+
+    @Test
+    void given_not_empty_pokemon_db_and_difference_between_db_and_network_when_fetch_the_list_then_db_should_be_cleared_and_fetch_should_happen() {
+        //given
+        FetchPokemonListUseCase fetchPokemonListUseCase = Mockito.mock(FetchPokemonListUseCase.class);
+        PokemonListItemRepository pokemonListItemRepository = Mockito.mock(PokemonListItemRepository.class);
+        PokeApiListNetworkRepository pokeApiListNetworkRepository = Mockito.mock(PokeApiListNetworkRepository.class);
+
+        PokemonListLoaderService pokemonListLoaderService = new PokemonListLoaderService(
+                pokeApiListNetworkRepository,
+                pokemonListItemRepository,
+                fetchPokemonListUseCase
+        );
+        PokeApiListResult pokeApiListResult = new PokeApiListResult();
+        pokeApiListResult.setCount(20);
+        Mockito.when(pokemonListItemRepository.count()).thenReturn(10L);
+        Mockito.when(pokeApiListNetworkRepository.fetchPokemonListResult(1, 0)).thenReturn(pokeApiListResult);
+
+        //when
+        pokemonListLoaderService.loadPokemonList();
+
+        //then
+        Mockito.verify(pokemonListItemRepository).deleteAll();
+        Mockito.verify(fetchPokemonListUseCase).execute();
+    }
+
+    @Test
+    void given_not_empty_pokemon_db_and_no_difference_between_db_and_network_when_fetch_the_list_then_nothing_should_happen() {
+        //given
+        FetchPokemonListUseCase fetchPokemonListUseCase = Mockito.mock(FetchPokemonListUseCase.class);
+        PokemonListItemRepository pokemonListItemRepository = Mockito.mock(PokemonListItemRepository.class);
+        PokeApiListNetworkRepository pokeApiListNetworkRepository = Mockito.mock(PokeApiListNetworkRepository.class);
+
+        PokemonListLoaderService pokemonListLoaderService = new PokemonListLoaderService(
+                pokeApiListNetworkRepository,
+                pokemonListItemRepository,
+                fetchPokemonListUseCase
+        );
+        PokeApiListResult pokeApiListResult = new PokeApiListResult();
+        pokeApiListResult.setCount(10);
+        Mockito.when(pokemonListItemRepository.count()).thenReturn(10L);
+        Mockito.when(pokeApiListNetworkRepository.fetchPokemonListResult(1, 0)).thenReturn(pokeApiListResult);
+
+        //when
+        pokemonListLoaderService.loadPokemonList();
+
+        //then
+        Mockito.verify(fetchPokemonListUseCase, Mockito.never()).execute();
+    }
 }
